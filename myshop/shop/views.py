38d12e2d3e8e5e25eaab28d8512a43acd53ebd1a
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2022-10-27 22:45:31 +0500

    load html

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+# Получение списка продуктов
+def product_list(request, category_slug=None):
+    category = None
+    # Получаем все категории товаров
+    categories = Category.objects.all()
+    # получаем все товары при этом фильтруем по коонке "доступность"
+    products = Product.objects.filter(available=True)
+    # Если получили в запросе category_slug вытаскиваем все продукты нужной категории 
+    if category_slug:
+        # Получаем категорию
+        category = get_object_or_404(Category, slug=category_slug)
+        # получаем продукты указанной категории
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+# Получение одного продукта
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
